339663d4dd349d322bd043a66e723c5b2f6bebd2
anthonySchafrik <anthonyschafrik@gmail.com>
2018-09-06 23:01:47 -0400

    'container class created'

---
+++ client/components/container.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>flying with pandas</h1>
+      </div>
+    )
+  }
+}
+
+exports.Container;
