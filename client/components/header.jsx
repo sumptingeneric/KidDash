72c46be5eebf99ccf9abc6ef49ee88a2fe22bab4
Justin Winton <justin.winton@gmail.com>
2018-09-06 20:04:42 -0700

    started header

---
+++ client/components/header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class header extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <div>Hamburger Menu</div>
+        <h1>Kid Dash</h1>
+        <div>Filter Bar</div>
+      </div>
+  }
+}
