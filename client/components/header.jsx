c7433f4f35229a18a4f4bb0b19e5f30ca649d5fa
Justin Winton <justin.winton@gmail.com>
2018-09-10 14:14:02 -0700

    individual button functions created

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom";
 
 
 class Header extends React.Component {
+  showAdmin() {
+    alert("Go to admin page");
+  }
   render() {
     return (
       <div className="Header">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/32px-Hamburger_icon.svg.png"></img>
+        <button onClick={this.showAdmin}><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/32px-Hamburger_icon.svg.png"></img></button>
         <h1>Kid Dash</h1>
       </div>
-    )
+    );
   }
-}
+};
+
 
 export default Header;
