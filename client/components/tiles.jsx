94f99d92e5af4b0f75c95d32c4cdc85bcd84c786
anthonySchafrik <anthonyschafrik@gmail.com>
2018-09-10 15:20:50 -0400

    'added tile list component render tile list to app.jsx updated tiles to render data'

--- client/components/tiles.jsx
+++ client/components/tiles.jsx
@@ -1,22 +1,23 @@
 import React, { Component } from "react";
 
 
-  function Tile(props) {
+
+const Tile = tile => { 
     return (
-      <div className="tile" title={props.catagory}>
-        <div className="date">{props.timeStamp}</div>
+      <div className="tile" title={tile.catagory}>
+        <div className="date">{tile.timeStamp}</div>
         <div className="linkimg">
-          <a href={props.doc_url}>
+          <a href={tile.doc_url}>
             <img
               className="imglink"
               alt="happycoding"
-              src={props.img_url}
+              src={tile.img_url}
               width="100"
               height="100"
             />
           </a>
         </div>
-        <h3 className="tileCaption">{props.caption}</h3>
+        <h3 className="tileCaption">{tile.caption}</h3>
       </div>
     );
   };
