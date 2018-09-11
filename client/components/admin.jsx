7c3f38ba1ebff6c1a3d9044f2f97befcc8f34500
anthonySchafrik <anthonyschafrik@gmail.com>
2018-09-11 14:16:24 -0400

    'send right props names to server for datebase'

--- client/components/admin.jsx
+++ client/components/admin.jsx
@@ -15,11 +15,16 @@ class Admin extends React.Component {
     this.handleClick = this.handleClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.setCategroy = this.setCategroy.bind(this);
+    this.setcaption = this.setcaption.bind(this);
 
   }
 
+  setcaption(event) {
+    this.setState({caption: event.target.value})
+  }
+
   setCategroy(event) {
-    this.setState({ categroy: event.target.value})
+    this.setState({categroy: event.target.value})
   }
 
   handleChange(event) {
@@ -29,10 +34,12 @@ class Admin extends React.Component {
   handleClick() {
     let fileInfo = {}
     fileInfo.url = this.state.value;
-    fileInfo.categroy = this.state.categroy
+    fileInfo.categroy = this.state.categroy;
+    fileInfo.caption = this.state.caption;
 
-    console.log(`i was clicked to send off-->: ${fileInfo.file}`);
-    console.log(`this selected item-->: ${fileInfo.categroy}`);
+    console.log(`i was clicked to send off url-->: ${fileInfo.url}`);
+    console.log(`this selected categroy-->: ${fileInfo.categroy}`);
+    console.log(`this selected caption-->: ${fileInfo.caption}`);
 
     axios.post('/api/docs', { fileInfo })
     .then(res => {
@@ -44,23 +51,23 @@ class Admin extends React.Component {
     return (
     <div>
 
-    <h2>Hello admin select which subject you like to upload to then click the submint button</h2>
+      <h2>Hello admin select which subject you like to upload to then click the submint button</h2>
 
-        Please input Document name -> <input type='text' value={this.state.caption}></input>
+        Please input Document name -> <input type='text' onChange={this.setcaption}></input>
 
-    Pick upload Categroy: <select onChange={this.setCategroy}>
-      <option value="Newsletter">Newsletter</option>
-      <option value="Sports">sports</option>
-      <option value="Teacher_Notes">Teacher_Notes</option>
-      <option value="FAQ">FAQ</option>
-      <option value="School_Events">School_Events</option>
-      <option value="After_School">After_School</option>
-    </select>
+      Pick upload Categroy: <select onChange={this.setCategroy}>
+        <option value="Newsletter">Newsletter</option>
+        <option value="Sports">sports</option>
+        <option value="Teacher_Notes">Teacher_Notes</option>
+        <option value="FAQ">FAQ</option>
+        <option value="School_Events">School_Events</option>
+        <option value="After_School">After_School</option>
+      </select>
 
-    URL for google Doc -> <input type="text" value={this.state.value} onChange={this.handleChange} placeholder={this.state.placeholder} />
-    <button onClick={this.handleClick}>SUBMINT</button>
+      URL for google Doc -> <input type="text" value={this.state.value} onChange={this.handleChange} placeholder={this.state.placeholder} />
+      <button onClick={this.handleClick}>SUBMINT</button>
 
-    Or upload file <input type='file'></input>
+      Or upload file <input type='file'></input>
     </div>
     )
   }
