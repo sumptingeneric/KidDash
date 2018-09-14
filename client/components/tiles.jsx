949a7819a8f9a951255a1bfa3c5b844a8f9e04f4
JBen13 <jessicabenjaminxiii@gmail.com>
2018-09-13 19:36:33 -0700

    add background image to project and html, styled tiles and tilesList

--- client/components/tiles.jsx
+++ client/components/tiles.jsx
@@ -1,28 +1,39 @@
 import React, { Component } from "react";
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 const moment = require('moment');
 
 
-const Tile = tile => { 
-    return (
-      <div className="tile" title={tile.category}>
-        <div className="date">{moment(tile.timeStamp).format("MMMM Do YYYY")}</div>
-        <div className="linkimg">
-          <a src={tile.doc_url}>
-            
-            <img
-              className="imglink"
-              alt="happycoding"
-              src={tile.img_url}
-              width="100"
-              height="100"
-            />
-          </a>
-        </div>
-        <h3 className="tileCaption">{tile.caption}</h3>
-      </div>
+
+
+const Tile = tile => {
+ 
+  return (
+    <Card title={tile.category} className="card" style={{height: 325, maxWidth: 345, justifyContent: "space-around"}} elevation={8}>
+      <CardActionArea style={{display:'flex', justifyContent: "space-around"}} >
+        <CardMedia className="media" height="140" component="img" style={{marginLeft: 15, marginTop: 18, objectFit: 'cover', justify: 'center', position: 'relative'}}image={tile.img_url}>
+        </CardMedia>
+      </CardActionArea>
+      <Typography style={{display:'flex', justifyContent: "space-around"}} gutterBottom variant="headline" component="h2">
+        {tile.caption}
+      </Typography>
+      <Typography style={{display:'flex', justifyContent: "space-around"}} component="p">
+        {moment(tile.timeStamp).format("MMMM Do YYYY")}
+      </Typography>
+      <CardActions>
+        <Button href={tile.doc_url} size="small" color="primary">
+          Open Document
+        </Button>
+      </CardActions>
+    </Card>
+        
     );
   };
-
 
 export default Tile;
 
