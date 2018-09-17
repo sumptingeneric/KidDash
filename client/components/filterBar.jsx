282ca287bd843ca0f4a3ea3ae5912189d20858b6
JBen13 <jessicabenjaminxiii@gmail.com>
2018-09-17 11:27:40 -0700

    Updates button styling on filterBar and allows background image to cover the entire length of the web app.

--- client/components/filterBar.jsx
+++ client/components/filterBar.jsx
@@ -5,12 +5,12 @@ import Button from '@material-ui/core/Button';
 const FilterBar = props => {
   return (
     <nav className="filterBar">
-      <Button variant="contained" style={{"marginLeft": 20}}color="primary" className="showAll" onClick={() => props.getFiles()} >Show All</Button>
-      <Button variant="contained" style={{"marginLeft": 20}}color="primary" className="newsletters" onClick={() => props.getFiles("Newsletters")} >Newsletters</Button>
-      <Button variant="contained" style={{"marginLeft": 20}}color="primary" className="sports" onClick={() => props.getFiles("Sports")} >Sports</Button>
-      <Button variant="contained" style={{"marginLeft": 20}}color="primary" className="teachersNotes" onClick={() => props.getFiles("Teachers_Notes")} >Teachers Notes</Button>
-      <Button variant="contained" style={{"marginLeft": 20}}color="primary" className="schoolEvents" onClick={() => props.getFiles("School_Events")} >School Events</Button>
-      <Button variant="contained" style={{"marginLeft": 20}}color="primary" className="faq" onClick={() => props.getFiles("FAQ")} >FAQ</Button>
+      <Button style={{"marginLeft": 20}}color="inherit" className="showAll" onClick={() => props.getFiles()} >Show All</Button>
+      <Button style={{"marginLeft": 20}}color="inherit" className="newsletters" onClick={() => props.getFiles("Newsletters")} >Newsletters</Button>
+      <Button style={{"marginLeft": 20}}color="inherit" className="sports" onClick={() => props.getFiles("Sports")} >Sports</Button>
+      <Button style={{"marginLeft": 20}}color="inherit" className="teachersNotes" onClick={() => props.getFiles("Teachers_Notes")} >Teachers Notes</Button>
+      <Button style={{"marginLeft": 20}}color="inherit" className="schoolEvents" onClick={() => props.getFiles("School_Events")} >School Events</Button>
+      <Button style={{"marginLeft": 20}}color="inherit" className="faq" onClick={() => props.getFiles("FAQ")} >FAQ</Button>
     </nav>
   );
 };
